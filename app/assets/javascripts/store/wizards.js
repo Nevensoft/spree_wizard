87760f7562ba6908616975bbe3ad16b55c981d90
Weston Platter <westonplatter@gmail.com>
2013-07-06 16:04:37 -0600

    example of how to add js and css in rails

--- app/assets/javascripts/store/wizards.js
+++ app/assets/javascripts/store/wizards.js
@@ -1,1 +1,15 @@
 //= require store/spree_frontend
+
+//
+// in order to have JS within a Rails application, you need to wrap the JS
+//   within an anynomous function, which is then executed as the file is 
+//   "rendered" to the page with the controller.
+//
+
+$(function(){
+
+  $(document).ready( function(){
+    alert('this is an example in regualr javascript');
+  });
+
+});
