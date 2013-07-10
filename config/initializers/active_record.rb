4dd958619991967bd098d790920be808d2a2346e
Weston Platter <westonplatter@gmail.com>
2013-07-10 02:06:41 -0600

    because of this, https://github.com/spree/spree/blob/e1e175c1cc1d27d8729991f344fcb4c7ad1d946a/core/lib/generators/spree/install/install_generator.rb#L103

--- config/initializers/active_record.rb
+++
@@ -1,5 +0,0 @@
-# Since we're using backbone, we need turn off the AR feature that wraps
-# ActiveRecord data in an extra json layer. More info on this here,
-# http://backbonejs.org/#FAQ-rails
-# 
-ActiveRecord::Base.include_root_in_json = false
