c3a851837850e13f9eaf3ae63e86b5cca71406c2
Weston Platter <westonplatter@gmail.com>
2013-07-09 18:37:41 -0600

    ensure the WizardsController returns json response

--- spec/controllers/spree/wizards_controller_spec.rb
+++ spec/controllers/spree/wizards_controller_spec.rb
@@ -2,30 +2,34 @@ require 'spec_helper'
 
 describe Spree::WizardsController do
 
-  # # This should return the minimal set of attributes required to create a valid
-  # # Wizard. As you add validations to Wizard, be sure to
-  # # adjust the attributes here as well.
-  # let(:valid_attributes) { { "name" => "MyString" } }
+  before :all do 
+    @wizard = FactoryGirl.create(:wizard)
+  end
 
-  # # This should return the minimal set of values that should be in the session
-  # # in order to pass any filters (e.g. authentication) defined in
-  # # WizardsController. Be sure to keep this updated too.
-  # let(:valid_session) { {} }
+  describe 'GET #index' do
+    it 'assigns all wizards as @wizards' do
+      get :index, use_route: :spree
+      assigns(:wizards).should eq([@wizard])
+    end
 
-  # describe "GET index" do
-  #   it "assigns all wizards as @wizards" do
-  #     wizard = Spree::Wizard.create! valid_attributes
-  #     get :index, use_route: :spree
-  #     assigns(:wizards).should eq([wizard])
-  #   end
-  # end
+    it 'returns json list' do 
+      xhr :get, :index, 
+        use_route: 'spree', format: 'json'
+      response.body.should == [@wizard].to_json
+    end
+  end
 
-  # describe "GET show" do
-  #   it "assigns the requested wizard as @wizard" do
-  #     wizard = Spree::Wizard.create! valid_attributes
-  #     get :show, use_route: :spree, {:id => wizard.to_param}
-  #     assigns(:wizard).should eq(wizard)
-  #   end
-  # end
+  describe 'GET #show' do
+    it 'assigns the requested wizard as @wizard' do
+      get :show, use_route: :spree, id: @wizard.to_param
+      assigns(:wizard).should eq(@wizard)
+    end
+
+    it 'returns json for wizard' do 
+      xhr :get, :show, id: @wizard.to_param,
+        use_route: 'spree', format: 'json'
+      response.body.should == @wizard.to_json
+    end
+  end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@ require 'rspec/rails'
 require 'database_cleaner'
 require 'ffaker'
 require 'shoulda'
+require 'factory_girl'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -34,6 +35,8 @@ require 'spree_product_selection_wizard/factories'
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+
+  config.include Devise::TestHelpers, type: :controller
 
   # == URL Helpers
   #
