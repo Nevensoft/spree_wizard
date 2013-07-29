5a65cfc71cf2222bc2bb1b848f165b686cd3f352
Weston Platter <westonplatter@gmail.com>
2013-07-28 18:16:41 -0600

    upgraded the Ruby to key: value pair convention

--- lib/generators/spree_wizard/install/install_generator.rb
+++ lib/generators/spree_wizard/install/install_generator.rb
@@ -2,7 +2,7 @@ module SpreeWizard
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
-      class_option :auto_run_migrations, :type => :boolean, :default => false
+      class_option :auto_run_migrations, type: :boolean, default: false
 
       def add_javascripts
         append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_wizard\n"
@@ -10,8 +10,9 @@ module SpreeWizard
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_wizard\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_wizard\n", :before => /\*\//, :verbose => true
+        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_wizard\n", before: /\*\//, verbose: true
+        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_wizard\n", before: /\*\//, verbose: true
+      end
       end
 
       def add_migrations
