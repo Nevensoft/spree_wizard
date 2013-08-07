b2198c70c121c3eef263fa042506555e18ea95dd
Weston Platter <westonplatter@gmail.com>
2013-08-06 18:15:30 -0700

    create wizard taxons so testing template rendering can evaluate valid taxon objects

--- spec/support/factories/wizards.rb
+++ spec/support/factories/wizards.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
   factory :wizard, class: Spree::Wizard do
     name 'complete package'
     permalink 'complete'
+    taxons { [FactoryGirl.create(:taxon), 
+                FactoryGirl.create(:taxon)] }
   end
 end
