c2a3b0b1fcaf7308c41c1458972c87074fe3172b
Weston Platter <westonplatter@gmail.com>
2013-09-20 19:30:47 -0600

    added Bullet gem to tune/update SQL to kill off N+1 queries

--- app/controllers/spree/wizards_controller.rb
+++ app/controllers/spree/wizards_controller.rb
@@ -39,15 +39,14 @@ module Spree
     # Get taxon and eager load associated objects
     # GET /wizards/taxon/1/products.json
     def taxon_products
-      @taxon = Spree::Taxon.find(params[:taxon_id])
-      # Completed 200 OK in 34ms (Views: 26.5ms | ActiveRecord: 1.1ms)
+      @taxon = Spree::Taxon.
+        includes(products: { variants: [:images, :default_price, :option_values]}).
+        includes(products: { master:   [:images, :default_price, :option_values]}).
+        find(params[:taxon_id])
       
-      # vs
-      # 
-      # @taxon = Spree::Taxon.includes(products: [variants: [:option_values]]).find(params[:taxon_id])
-      # Completed 200 OK in 224ms (Views: 165.6ms | ActiveRecord: 6.0ms)
-      
-      render 'spree/wizards/taxon_products_api', formats: :json, object: @taxon
+      render 'spree/wizards/taxon_products_api', 
+        formats: :json, 
+        object: @taxon
     end
 
   end
--- spree_wizard.gemspec
+++ spree_wizard.gemspec
@@ -33,4 +33,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'simplecov'
   s.add_development_dependency 'sqlite3'
   s.add_development_dependency 'shoulda'
+  s.add_development_dependency 'bullet'
+  s.add_development_dependency 'ruby-growl'
 end
