d7666baeaa416a4efa0cc9df532aa99b9b4bd0cb
Weston Platter <westonplatter@gmail.com>
2013-10-18 15:29:41 -0600

    fixed nested attr_accessible rerferences

--- app/models/spree/wizard.rb
+++ app/models/spree/wizard.rb
@@ -1,10 +1,9 @@
 module Spree
-  class Wizard < ActiveRecord::Base
-    attr_accessible :name, :taxon_ids, :permalink
-    
-    has_many :wizard_taxons, order: 'position ASC', class_name: 'Spree::WizardTaxon'
+  class Wizard < ActiveRecord::Base   
+    has_many :wizard_taxons, -> { order 'position ASC'}, 
+      class_name: 'Spree::WizardTaxon'
+      
     accepts_nested_attributes_for :wizard_taxons, allow_destroy: true
-    attr_accessible :wizard_taxons_attributes
     
     has_many :taxons, through: :wizard_taxons
   end
